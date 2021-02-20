85179121497d0dfc9a6446bafeb29f4929544081
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-02-20 15:21:21 +0100

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Nikola Anastasovski on 20.2.21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
